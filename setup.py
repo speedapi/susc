b83db168f20cde82570371d2a51f8d212e9d019d
Anna Antonenko <hi@portasynthinca3.me>
2022-03-31 22:30:12 +0300

    cd: add workflow to publish pkg

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from setuptools import setup
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+with open("requirements.txt", "r") as f:
+    REQUIREMENTS = f.read().splitlines()
+
 setup(
     name="susc",
-    version="0.8.5",
+    version="0.8.6",
     description="AMOGUS SUS description language compiler",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -16,7 +19,7 @@ setup(
     license="BSD 3-Clause",
     packages=["susc"],
     keywords=["api", "protocol"],
-    install_requires=["lark", "colorama", "markdown", "watchgod", "nanoid"],
+    install_requires=REQUIREMENTS,
     include_package_data=True,
     entry_points = {
         'console_scripts': ['susc=susc.__main__:main'],
