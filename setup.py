6f5e8ca3dece053964563ddf50ae8d585bfee868
Anna Antonenko <hi@portasynthinca3.me>
2022-06-04 15:51:38 +0300

    fix: bump ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="susc",
-    version="1.5.2",
+    version="1.5.3",
     description="SpeedAPI schema compiler",
     long_description=README,
     long_description_content_type="text/markdown",
